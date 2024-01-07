6cd65801edfc2459654fe6db45781b65a4847074
yusifk88 <amloyal4life@gmail.com>
2024-01-07 02:14:42 +0000

    first commit

---
+++ app/components/Details.vue
@@ -0,0 +1,52 @@
+<template>
+  <Page>
+    <ActionBar :title="item.title"/>
+
+    <ScrollView height="100%">
+
+      <StackLayout>
+        <Image margin="0" stretch="aspectFill" :src="item.image"></Image>
+
+        <StackLayout
+            padding="10 20"
+        >
+          <StackLayout v-for="detail in item.details" :key="detail.id">
+            <Label
+            marginTop="15"
+            fontSize="16"
+            fontWeight="700"
+            class="text-primary"
+            textWrap="true"
+            :text="detail.title"
+            />
+
+            <Label
+              fontSize="14"
+              class="text-primary"
+              textWrap="true"
+              :text="detail.body"
+
+            />
+
+          </StackLayout>
+
+        </StackLayout>
+      </StackLayout>
+
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+import Vue from 'nativescript-vue'
+
+export default Vue.extend({
+  props: ['item'],
+  name: "Details"
+})
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
